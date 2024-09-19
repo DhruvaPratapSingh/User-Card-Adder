0f79147704b8735ddd194dd7da1a8b1b68ef6c36
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2024-09-19 10:34:32 +0530

    fix

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use('/api/users', userRoutes);
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
